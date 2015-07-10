af799f8129af09b6267dc685894ae179e5ba6f5c
cablej <jackhcable@gmail.com>
2015-07-10 13:29:40 -0500

    Linked view controllers

---
+++ Divvy Marathon/LocationViewController.swift
@@ -0,0 +1,37 @@
+//
+//  LocationViewController.swift
+//  Divvy Marathon
+//
+//  Created by Jack Cable on 7/10/15.
+//  Copyright © 2015 Jack Cable. All rights reserved.
+//
+
+import UIKit
+
+class LocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("This will print the current location.")
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
