8bf240fc0b4ea21387697ec2f052302bb430e28b
cablej <jackhcable@gmail.com>
2015-07-10 12:20:49 -0500

    Added Station class

---
+++ Divvy Marathon/Station.swift
@@ -0,0 +1,13 @@
+//
+//  Station.swift
+//  Divvy Marathon
+//
+//  Created by Jack Cable on 7/10/15.
+//  Copyright © 2015 Jack Cable. All rights reserved.
+//
+
+import UIKit
+
+class Station: NSObject {
+
+}
