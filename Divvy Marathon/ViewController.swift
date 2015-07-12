9502a0c7163c499b54233651aca38586681e674f
Julianne <julesk1998@gmail.com>
2015-07-11 20:07:27 -0500

    added functions and instance variables to viewController for calculating the number of miles based on the number of minutes entered and the number of needed stops between the start and end of a trip based on the number of minutes

--- Divvy Marathon/ViewController.swift
+++ Divvy Marathon/ViewController.swift
@@ -12,6 +12,14 @@ class ViewController: UIViewController {
 
     var numMinutes = 0
     let dateFormatter = NSDateFormatter()
+    var numMiles = 0.0
+    /*
+    estimated average speed fo a bike in the city: ~6mph accoring to treehuggers.com based on data in lyons france.
+    we definitely need to look into this, maybe make it adjustable
+    */
+    var mph = 6.0
+    var numNeccessaryIntermediaryStops = 0 //doesn't include start and end stations
+    var tryToHitAsManyBikesAsPossible = false
     
     @IBOutlet weak var datePicker: UIDatePicker!
     
@@ -30,6 +38,28 @@ class ViewController: UIViewController {
         print(numMinutes)
     }
     
+    
+    
+
+    /**
+        uses the numMinutes to calculate numMiles based on the estimated average speed of a bike in the city
+    **/
+    func minutesToMiles(){
+        numMiles = mph / Double(60) * Double(numMinutes)
+    }
+    
+    /**
+        uses numMinutes to calculate numNecessaryIntermediaryStops, assumes 25 mins between stops
+    **/
+    func calculateNumNecessaryStops(){
+        var minsExcludingStartEndTimes = numMinutes - 25 // extra time not covered by first rental
+        if (minsExcludingStartEndTimes % 25 > 0) {
+            numNeccessaryIntermediaryStops = minsExcludingStartEndTimes / 25
+        }
+        else {
+            numNeccessaryIntermediaryStops = minsExcludingStartEndTimes / 25 - 1
+        }
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
